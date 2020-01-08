813d538f5c6540b60305435798e2375dad750d75
hackernovice <kev@hackernovice.com>
2020-01-08 17:15:07 -0500

    added options for disabling llmnr or nbns scanning

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ import nbns
 import llmnr
 
 logger.info("Starting up")
-nbns.init()
-llmnr.init()
+
+if str(os.environ['DISABLE_NBNS_SCANNING']).lower()[0] != 't':
+    nbns.init()
+if str(os.environ['DISABLE_LLMNR_SCANNING']).lower()[0] != 't':
+    llmnr.init()
