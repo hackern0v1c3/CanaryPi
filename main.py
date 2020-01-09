356b3c39d1f682b76ec5ba30a09c52a1e1ca9ea7
hackernovice <kev@hackernovice.com>
2020-01-09 17:13:10 -0500

    added alert handler to main script

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ import logger
 import nbns
 import llmnr
 import emailer
+import alert_handler
 
 logger.info("Starting up")
+
+alert_handler.init()
 
 # If enabled send a test email at program startup
 if str(os.environ['ENABLE_EMAIL_STARTUP_TEST']).lower()[0] == 't':
@@ -19,3 +22,4 @@ if str(os.environ['DISABLE_NBNS_SCANNING']).lower()[0] != 't':
 # If enabled start llmnr spoof detection
 if str(os.environ['DISABLE_LLMNR_SCANNING']).lower()[0] != 't':
     llmnr.init()
+
