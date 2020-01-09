0b847dc5670e99d6a10fd09a7b9753c92cfb7c77
hackernovice <kev@hackernovice.com>
2020-01-09 14:35:37 -0500

    added startup test email to main script

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 # Import packages
+import os
 import logger
 import nbns
 import llmnr
+import emailer
 
 logger.info("Starting up")
 
+# If enabled send a test email at program startup
+if str(os.environ['ENABLE_EMAIL_STARTUP_TEST']).lower()[0] == 't':
+    logger.debug("Attempting to send startup test email...")
+    emailer.send_email("CanaryPi startup", "CanaryPi is starting up.  Your email settings have been configured correctly.")
+
+# If enabled start netbios spoof detection
 if str(os.environ['DISABLE_NBNS_SCANNING']).lower()[0] != 't':
     nbns.init()
+
+# If enabled start llmnr spoof detection
 if str(os.environ['DISABLE_LLMNR_SCANNING']).lower()[0] != 't':
     llmnr.init()
