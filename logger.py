440dbb3aba7f14cb07ceff24df08f005d20131b3
hackernovice <kev@hackernovice.com>
2020-01-14 16:02:11 -0500

    added syslog support

--- logger.py
+++ logger.py
@@ -1,7 +1,9 @@
 # Import packages
 import os
 import logging
+import socket
 from logging.handlers import TimedRotatingFileHandler
+from logging.handlers import SysLogHandler
 
 # Make sure the log folder exists
 os.makedirs('logs',exist_ok=True)
@@ -25,6 +27,7 @@ def validate_log_level(level):
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 file_logger = logging.getLogger('Rotating Log by Day')
 console_logger = logging.getLogger('Log to Console')
+syslog_logger = logging.getLogger('Syslog')
 
 # Verify a valid logging level was provided for console logging
 console_log_level = str(os.environ['CONSOLE_LOG_LEVEL']).upper()
@@ -45,6 +48,14 @@ else:
     console_logger.critical("Invalid log level provided for file logging.  Must be debug, info, warning, error, or critical")
     exit(1)
 
+# Verify a valid logging level was provided for syslog logging
+syslog_log_level = str(os.environ['SYSLOG_LOG_LEVEL']).upper()
+if validate_log_level(syslog_log_level):
+    syslog_logger.setLevel(syslog_log_level)
+else:
+    console_logger.critical("Invalid log level provided for syslog logging.  Must be debug, info, warning, error, or critical")
+    exit(1)
+
 # Setup file logging options
 try:
     log_retention = int(os.environ['FILE_LOG_RETENTION'])
@@ -55,23 +66,62 @@ except:
     console_logger.critical("Invalid log retention provided.  Must be int")
     exit(1)
 
-# Setup functions for easily logging to console and file at the same time.
+# Setup syslog logging options
+if os.environ['SYSLOG_ENABLED'][0].lower() == 't':
+    try:
+        syslog_port = int(os.environ['SYSLOG_PORT'])
+    except:
+        console_logger.critical("Invalid syslog port provided.  Must be int")
+        exit(1)
+
+    if os.environ['SYSLOG_ADDRESS'] == '':
+        console_logger.critical("You must provide an ip address for the syslog server")
+        exit(1)
+
+    syslog_server = os.environ['SYSLOG_ADDRESS']
+    syslog_socktype = socket.SOCK_DGRAM
+
+    # Check if using TCP for syslog
+    if os.environ['SYSLOG_TCP'][0].lower() == 't':
+        syslog_socktype = socket.SOCK_STREAM
+
+    # Connections can fail if using TCP handler
+    try:
+        syslog_handler = SysLogHandler(address=(syslog_server, syslog_port), socktype=syslog_socktype)
+    except:
+        console_logger.critical("Unable to connect to syslog server")
+        exit(1)
+
+    syslog_handler.setFormatter(formatter)
+    syslog_logger.addHandler(syslog_handler)
+
+# Setup functions for easily logging to console, file, and syslog at the same time.
 def debug(message):
     console_logger.debug(message)
     file_logger.debug(message)
+    if os.environ['SYSLOG_ENABLED'][0].lower() == 't':
+        syslog_logger.debug(message)
 
 def info(message):
     console_logger.info(message)
     file_logger.info(message)
+    if os.environ['SYSLOG_ENABLED'][0].lower() == 't':
+        syslog_logger.info(message)
 
 def warning(message):
     console_logger.warning(message)
     file_logger.warning(message)
+    if os.environ['SYSLOG_ENABLED'][0].lower() == 't':
+        syslog_logger.warning(message)
 
 def error(message):
     console_logger.error(message)
     file_logger.error(message)
+    if os.environ['SYSLOG_ENABLED'][0].lower() == 't':
+        syslog_logger.error(message)
 
 def critical(message):
     console_logger.critical(message)
     file_logger.critical(message)
+    if os.environ['SYSLOG_ENABLED'][0].lower() == 't':
+        syslog_logger.critical(message)
