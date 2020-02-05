2816e0ebda547e7774346e891b1193643abbba83
hackernovice <kev@hackernovice.com>
2020-02-05 10:53:33 -0500

    updated mdns detection to prevent false positives

--- mdns.py
+++ mdns.py
@@ -2,11 +2,14 @@
 
 # Import modules
 from scapy.all import *
+from collections import deque
 import threading
 import random
 import string
 import logger
 import alert_handler
+
+spoofed_names = deque([], 10)
 
 # Function to generate random hostnames
 # Used microsofts DNS naming conventions https://support.microsoft.com/en-us/help/909264/naming-conventions-in-active-directory-for-computers-domains-sites-and
@@ -31,21 +34,17 @@ def sender():
         #source_port = random.randint(49152, 65535)
         #id = random.randint(1, 65535)
         query_name = generate_name()
-        
+
+        spoofed_names.append(query_name)
+        spoofed_names.append(query_name+'.')
+
         pkt = Ether()/IP(dst=DESTINATION_IP, ttl=1)/UDP(sport=5353,dport=5353)/DNS(id=0, qr=0, qdcount=1, ancount=0, nscount=0, rd=1, arcount=0, qd=DNSQR(qname=query_name))
 
         logger.debug(f'Sending MDNS spoofed packet')
-        ans=srp1(pkt,verbose=0,timeout=2)
-
-        if ans is None:
-            continue
-        elif ans.haslayer(ICMP):
-            continue
-        else:
-            got_packet(ans)
+        sendp (pkt, verbose=0)
 
 # Handler for incoming responses
-def got_packet(pkt):
+def get_packet(pkt):
     # rfc https://tools.ietf.org/html/rfc6762#section-6
     # response source port must be 5353
     # destination port must be 5353
@@ -60,18 +59,27 @@ def got_packet(pkt):
     src_port = pkt.getlayer(IP).sport
     src_mac = pkt.getlayer(Ether).src
 
-    if (dst_ip == '224.0.0.251') & (dst_port == 5353) & (src_port == 5353) & (pkt.qr == 1) & (pkt.opcode == 0) & (pkt.rcode == 0):
+    if (pkt.qr == 1) & (pkt.opcode == 0) & (pkt.rcode == 0):
+        
+
         # Get the machine name from the mdns response
         response_name = str(DNSRR(pkt.an[0]).rrname, 'utf-8')
 
-        logger.warning(f'A spoofed MDNS response for {response_name} was detected by from host {src_ip} - {src_mac}')
+        if response_name in spoofed_names:
+            logger.warning(f'A spoofed MDNS response for {response_name} was detected by from host {src_ip} - {src_mac}')
 
-        # Send message to alert handler
-        alert_handler.new_alert("mdns", src_ip, src_mac, f'MDNS response for {response_name}')
+            # Send message to alert handler
+            alert_handler.new_alert("mdns", src_ip, src_mac, f'MDNS response for {response_name}')
+
+#Function for starting sniffing
+def listen():
+    sniff(filter="udp and dst port 5353 and dst host 224.0.0.251",store=0,prn=get_packet)
 
 # Main function
 def init():
     try:
+        logger.info("Starting MDNS UDP Response Server...")
+        threading.Thread(target=listen).start()
         logger.info("Starting MDNS Request Thread...")
         threading.Thread(target=sender).start()
     except:
